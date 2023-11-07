818c48feadfc47bf26a6b8abdcf29b2f77162081
yeomsungpil <present7256@gmail.com>
2023-11-07 22:37:33 +0900

    iTunes API 활용 rx + input, output

--- SeSACRxThreads/iTunesSearchMiniApp/ITunesViewModel.swift
+++ SeSACRxThreads/iTunesSearchMiniApp/ITunesViewModel.swift
@@ -20,18 +20,46 @@ class ITunesViewModel {
 
     struct Input {
         let zip : Observable<(ControlEvent<IndexPath>.Element, ControlEvent<AppInfo>.Element)>
-        
+         let searchClicked: ControlEvent<Void>
+        let text: ControlProperty<String>
     }
     
     struct Output {
         let zip: Observable<ControlEvent<AppInfo>.Element>
+        let items: PublishSubject<[AppInfo]>
+        let search: Observable<[AppInfo]>
     }
     
     func transform(input: Input) -> Output {
         
         let zip = input.zip
-            .map { $0.1}
+            .map { $0.1 }
         
-        return Output(zip: zip)
+        let search = input.searchClicked
+            .withLatestFrom(input.text)
+            .throttle(RxTimeInterval.seconds(3), scheduler: MainScheduler.instance)
+            .debug()
+            //.distinctUntilChanged()
+            .flatMap {
+                SearchAPIManager.requestSearch(searchString: $0.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)
+            }
+            .map { $0.results }
+        
+        
+        
+        return Output(zip: zip, items: items, search: search)
+    }
+    
+    init() {
+        /// 네트워크 통신 - 초기값 세팅
+        let request = SearchAPIManager
+            .requestSearch(searchString: searchText.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)
+            .asDriver(onErrorJustReturn: SearchAppModel(resultCount: 0, results: []))
+        
+        request
+            .drive(with: self) { owner, response in
+                owner.items.onNext(response.results)
+            }
+            .disposed(by: disposeBag)
     }
 }
--- SeSACRxThreads/iTunesSearchMiniApp/iTunesSearchMiniViewController.swift
+++ SeSACRxThreads/iTunesSearchMiniApp/iTunesSearchMiniViewController.swift
@@ -39,11 +39,11 @@ class ITunesSearchMiniViewController : UIViewController {
     func bind() {
         
         
-        let input = ITunesViewModel.Input(zip: Observable.zip(tableView.rx.itemSelected, tableView.rx.modelSelected(AppInfo.self)))
+        let input = ITunesViewModel.Input(zip: Observable.zip(tableView.rx.itemSelected, tableView.rx.modelSelected(AppInfo.self)), searchClicked: searchController.searchBar.rx.searchButtonClicked, text: searchController.searchBar.rx.text.orEmpty)
         
         let output = viewModel.transform(input: input)
         ///TableView에 데이터 뿌려주기
-        viewModel.items
+        output.items
             .bind(to: tableView.rx.items(cellIdentifier: ITunesTableViewCell.identifier, cellType: ITunesTableViewCell.self)) { row, element, cell in
                 
                 cell.setUI(data: element)
@@ -72,28 +72,29 @@ class ITunesSearchMiniViewController : UIViewController {
         
         
         /// 네트워크 통신 - 초기값 세팅
-        let request = SearchAPIManager
-            .requestSearch(searchString: viewModel.searchText.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)
-            .asDriver(onErrorJustReturn: SearchAppModel(resultCount: 0, results: []))
-        
-        request
-            .drive(with: self) { owner, response in
-                owner.viewModel.items.onNext(response.results)
-            }
-            .disposed(by: disposeBag)
+//        let request = SearchAPIManager
+//            .requestSearch(searchString: viewModel.searchText.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)
+//            .asDriver(onErrorJustReturn: SearchAppModel(resultCount: 0, results: []))
+//
+//        request
+//            .drive(with: self) { owner, response in
+//                owner.viewModel.items.onNext(response.results)
+//            }
+//            .disposed(by: disposeBag)
             
         
         
         /// 서치 검색 클릭 후 네트워크 통신
-        searchController.searchBar.rx.searchButtonClicked
-            .withLatestFrom(searchController.searchBar.rx.text.orEmpty)
-            .throttle(RxTimeInterval.seconds(3), scheduler: MainScheduler.instance)
-            .debug()
-            //.distinctUntilChanged()
-            .flatMap {
-                SearchAPIManager.requestSearch(searchString: $0.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)
-            }
-            .map { $0.results }
+//        searchController.searchBar.rx.searchButtonClicked
+//            .withLatestFrom(searchController.searchBar.rx.text.orEmpty)
+//            .throttle(RxTimeInterval.seconds(3), scheduler: MainScheduler.instance)
+//            .debug()
+//            //.distinctUntilChanged()
+//            .flatMap {
+//                SearchAPIManager.requestSearch(searchString: $0.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)
+//            }
+//            .map { $0.results }
+            output.search
             .bind(with: self, onNext: { owner, value in
                 owner.viewModel.items.onNext(value)
             })
